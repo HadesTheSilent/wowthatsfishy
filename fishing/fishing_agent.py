17784ab7290d3a4007f1566bcad0da0572708f10
Hades <jsallesjf@hotmail.com>
2025-07-10 21:47:39 -0300

    Mudanças no main agent, cursor move até a isca

---
+++ fishing/fishing_agent.py
@@ -0,0 +1,45 @@
+import cv2 as cv
+import numpy as np
+import pyautogui
+import time
+
+class FishingAgent:
+    def __init__(self, main_agent) -> None:
+        self.main_agent = main_agent
+        self.fishing_target = cv.imread("fishing/assets/fishing_target.png")
+        self.fishing_thread = None
+
+    def cast_lure(self):
+        print("Pescando...")
+        # pyautogui.press('1')
+        time.sleep(2)
+        self.find_lure()
+
+    def find_lure(self):
+        while self.main_agent.cur_img is None or not isinstance(self.main_agent.cur_img, np.ndarray):
+            print("Aguardando captura de tela...")
+            time.sleep(0.1)
+        cur_img = self.main_agent.cur_img
+        lure_location = cv.matchTemplate(
+            cur_img, 
+            self.fishing_target,
+            cv.TM_CCOEFF_NORMED)
+        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(lure_location)
+        print(max_loc)
+        self.move_to_lure(max_loc)
+        # cv.imshow("Match Template", lure_location)
+        # cv.waitKey(0)
+
+    def move_to_lure(self, max_loc):
+        pyautogui.moveTo(max_loc[0], max_loc[1], 1, pyautogui.easeOutQuad)
+
+    def watch_lure(self, max_loc):
+        pass
+
+    def pull_line(self):
+        pass
+
+    def run(self):
+        while True:
+            self.cast_lure()
+            time.sleep(5)
--- main.py
+++ main.py
@@ -1,22 +1,86 @@
+
+
 from PIL import Image, ImageGrab
-import numpy as np 
+import numpy as np
 import cv2 as cv
 import time
+from threading import Thread
+from fishing.fishing_agent import FishingAgent
 
-def update_screen():
+
+class MainAgent:
+    def __init__(self) -> None:
+        self.agents = []
+        self.fishing_thread = None
+
+        self.cur_img = None     # BGR Image
+        self.cur_imgHSV = None  # HSV Image
+
+        self.zone = "Darkmoon Faire"
+        self.time = "night"
+        
+        # print("MainAgent setup complete...")
+
+
+def update_screen(agent):
+
     t0 = time.time()
     while True:
-        screenshot = ImageGrab.grab()
-        screenshot_np = np.array(screenshot)
-        screenshot = cv.cvtColor(screenshot_np, cv.COLOR_RGB2BGR)
+        agent.cur_img = ImageGrab.grab()
+        agent.cur_img = np.array(agent.cur_img)
+        agent.cur_img = cv.cvtColor(agent.cur_img, cv.COLOR_RGB2BGR)
+        agent.cur_imgHSV = cv.cvtColor(agent.cur_img,cv.COLOR_BGR2HSV)
 
-        cv.imshow("Computer Vision", screenshot)
+        # Scv.imshow("Computer Vision", agent.cur_img)
         key = cv.waitKey(1)
-        if key == ord ('q'):
+        if key == ord('q'):
             break
         ex_time = time.time() - t0
-        print("FPS: " + str(1 / ex_time))
+        # print("FPS: " + str(1 / (ex_time)))
         t0 = time.time()
-       
+
+
+def print_menu():
+    print("Escolha um comando:")
+    print("\tS\tStart the main agent.")
+    print("\tZ\tEscolha a zona.")
+    print("\tF\tComeçar a pescar.")
+    print("\tQ\tSair.")
+
+
 if __name__ == "__main__":
-    update_screen()
+    main_agent = MainAgent()
+
+    print_menu()
+    while True:
+        user_input = input()
+        user_input = str.lower(user_input).strip()
+
+        if user_input == "s":
+            update_screen_thread = Thread(
+            target=update_screen, 
+            args=(main_agent,),
+            name="update screen thread",
+            daemon=True)
+            update_screen_thread.start()
+            print("Thread started")
+
+        elif user_input =='z':
+            pass
+
+        elif user_input == 'f':
+            fishing_agent = FishingAgent(main_agent)
+            fishing_agent.run()
+
+        elif user_input == 'q':
+            cv.destroyAllWindows()
+            break
+
+        else:
+            print("Input error.")
+            print_menu()
+
+    print("Done.")
+
+
+ 
